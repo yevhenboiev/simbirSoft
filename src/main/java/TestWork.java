bff07bbcfe1f32af3e549b5f9cd98222b8a08243
yevhenboiev@gmail.com <zipzone12345>
2021-11-04 15:51:19 +0300

    Make log4j2 xml, add logger in JsoupParser

--- src/main/java/TestWork.java
+++ src/main/java/TestWork.java
@@ -3,6 +3,7 @@ import com.simbirsoft.testwork.model.Program;
 public class TestWork {
     public static void main(String[] args) {
         Program program = new Program("http://www.girobordi.ru/");
+
         program.startProgram();
     }
 }
--- src/main/java/com/simbirsoft/testwork/util/JsoupParser.java
+++ src/main/java/com/simbirsoft/testwork/util/JsoupParser.java
@@ -1,12 +1,16 @@
 package com.simbirsoft.testwork.util;
 
 import com.simbirsoft.testwork.exeption.NotFoundUrl;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 import java.io.IOException;
 
 public class JsoupParser {
+
+    private static Logger logger = LogManager.getLogger(JsoupParser.class);
     private String url;
 
     public JsoupParser(String url) {
@@ -18,6 +22,7 @@ public class JsoupParser {
             Document webSite = Jsoup.connect(url).get();
             return webSite.text();
         } catch (IOException e) {
+            logger.info("Not found url {}", url);
             throw new NotFoundUrl(url);
         }
     }
